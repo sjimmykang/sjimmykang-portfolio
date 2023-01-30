a183aa65127860c48c55c0cb9693efe0aa282e29
Jimmy Kang <sjimmy.kang@gmail.com>
2023-01-30 06:25:27 -0500

    project complete

---
+++ nav.js
@@ -0,0 +1,18 @@
+/* Navigation bar Scrolling down to hide, up to show */
+/* got the info on how to from: https://www.cssscript.com/show-hide-navbar-scroll-down-up/ 
+and modified to fit my need. */
+
+const navEl = document.querySelector('nav');
+let lastScroll = 0;
+
+/* listens to scroll and compare to last scroll to current scroll to determine scroll direction */
+window.addEventListener('scroll', () => {
+    let currentScroll = window.scrollY;
+
+    if (currentScroll - lastScroll > 0) {
+        navEl.classList.add('scroll-down');
+    } else {
+        navEl.classList.remove('scroll-down');
+    }
+    lastScroll = currentScroll;
+})
